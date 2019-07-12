fa55e47a16c29b7424fc81978444f7bf7b070eb2
kokrui <h1410129@nushigh.edu.sg>
2019-07-12 17:26:24 +0800

    idk what im doing but login setup???

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var CompanyUser = require('../lib/CompanyUser');
+//var NomadUser = require('../lib/NomadUser');
+
+router.get('/', function(req,res,next){
+	res.render('index', {title:'Express'});
+});
+
+router.post('/comregister', function(req,res){
+	var username=req.body.username;
+	var password=req.body.password;
+	var name=req.body.name;
+	
+	var newcom=new CompanyUser();
+	newcom.username=username;
+	newcom.password=password;
+	newcom.name=name;
+	newcom.save(function(err,savedCom){
+		if(err){
+			console.log(err);
+			return res.status(500).send();
+		}
+		return res.status(200).send();
+});
