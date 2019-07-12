cfe2af7d4932f6a88cec23ea6a7fee00a62a011b
Xavier <xavieroh05@gmail.com>
2019-07-12 17:31:50 +0800

    Login page

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 
-
 app.set('view engine', 'ejs');
 
 app.use('/', express.static('./public'));
 
-
 app.get('/', (req, res) => {
-  res.render('pages/index', {foo: 'FOO'});
+  res.render('pages/index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('pages/login');
 });
 
 var routes = require('./routes/index');
