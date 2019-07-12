5c83c7fd4d828d3bbd77d83226bb3396e3e10145
kokrui <h1410129@nushigh.edu.sg>
2019-07-12 19:47:09 +0800

    projlist again idk why jn didnt work when i push

--- routes/index.js
+++ routes/index.js
@@ -9,34 +9,10 @@ router.get('/profile', (req, res) => {
   res.render('pages/profile', {isAuthenticated: true});
 });
 
-<<<<<<< HEAD
 router.get('/projlist', (req, res) => {
   res.render('pages/projlist', {isAuthenticated: true});
 });
 
-=======
-<<<<<<< HEAD
-router.post('/comregister', function(req,res){
-	var username=req.body.username;
-	var password=req.body.password;
-	var name=req.body.name;
-
-	var newcom=new CompanyUser();
-	newcom.username=username;
-	newcom.password=password;
-	newcom.name=name;
-	newcom.save(function(err,savedCom){
-		if(err){
-			console.log(err);
-			return res.status(500).send();
-		}
-		return res.status(200).send();
-	});
-});
-
-=======
->>>>>>> 55cb7c4c3abe4ee2504832f0893976a70e75edcb
->>>>>>> ed82ef91a57473770d059f7de375b6608461a96e
 // function ensureAuthenticated(req, res, next) {
 // 	if (req.isAuthenticated()) {
 // 		return next();
