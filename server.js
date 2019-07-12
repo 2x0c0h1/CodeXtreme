ba5cfd8f5074cc02e3c694106d9c94be2d0f4f70
Xavier <xavieroh05@gmail.com>
2019-07-12 17:03:29 +0800

    Added additional routing

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.use('/', express.static('./public'));
 
 app.set('view engine', 'ejs');
+
+app.use('/', express.static('./public'));
+
 
 app.get('/', (req, res) => {
   res.render('pages/index', {foo: 'FOO'});
 });
+
+var routes = require('./routes/index');
+
+app.use('/', routes);
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) { return next(); }
+//   res.redirect('/login');
+// };
 
 app.listen(8080, () => console.log("Listening on port 8080!"));
